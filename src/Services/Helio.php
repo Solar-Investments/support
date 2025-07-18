5c3b323b824f71dcd7f6454b6071098e38eb9c00
Kyle Anderson <kyle.anderson@fixr.com>
2025-07-18 10:31:03 -0400

    Configure logging

--- src/Services/Helio.php
+++ src/Services/Helio.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace SolarInvestments\Services;
 
+use Illuminate\Config\Repository;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Foundation\Bootstrap\HandleExceptions;
 use Illuminate\Foundation\Bootstrap\LoadConfiguration;
+use Monolog\Formatter\GoogleCloudLoggingFormatter;
+use Monolog\Handler\StreamHandler;
+use Monolog\Processor\PsrLogMessageProcessor;
 
 class Helio
 {
@@ -22,9 +26,35 @@ class Helio
                 //
             },
             HandleExceptions::class => static function () use ($app): void {
-                //
+                static::configureLogging($app);
             },
             default => static fn () => true,
         })();
+    }
+
+    public static function configureLogging(Application $app): void
+    {
+        if ($app->isLocal()) {
+            return;
+        }
+
+        /** @var Repository $config */
+        $config = $app['config'];
+
+        $config->set('logging.default', 'stderr');
+
+        $config->set('logging.channels.stderr', [
+            'driver' => 'monolog',
+            'level' => $app->isProduction() ? 'warning' : 'debug',
+            'handler' => StreamHandler::class,
+            'handler_with' => [
+                'stream' => 'php://stderr',
+            ],
+            'formatter' => GoogleCloudLoggingFormatter::class,
+            'formatter_with' => [
+                'includeStacktraces' => true,
+            ],
+            'processors' => [PsrLogMessageProcessor::class],
+        ]);
     }
 }
