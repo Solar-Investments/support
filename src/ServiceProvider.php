6451dff9933fefbc161aba7cc2e8d9748a7f6547
Kyle Anderson <kyle.anderson@fixr.com>
2025-07-22 08:40:58 -0400

    Revert "Conditional Redis clustering"
    This reverts commit 3b07438a
    This reverts commit a54cf997

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -12,7 +12,6 @@ class ServiceProvider extends BaseServiceProvider
     {
         $this->app->register(Providers\MacroServiceProvider::class);
         $this->app->register(Providers\MiddlewareServiceProvider::class);
-        $this->app->register(Providers\RedisServiceProvider::class);
         $this->app->register(Providers\UrlServiceProvider::class);
     }
 }
