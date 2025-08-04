2abafedb91e18e964310d216f321de918a558f80
Kyle Anderson <kyle.anderson@fixr.com>
2025-08-04 13:37:29 -0400

    Prepend `git pull` to command sequence

--- src/Services/Helio.php
+++ src/Services/Helio.php
@@ -49,6 +49,7 @@ class Helio
         $project = Env::get('GCP_PROJECT_ID', 'helio-platform');
 
         $config->set('statamic.git.commands', [
+            '{{ git }} pull',
             '{{ git }} add {{ paths }}',
             collect([
                 '{{ git }}',
